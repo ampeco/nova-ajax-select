5a6b1f3cf03097afa9f11a40374f95a5ded451db
Nikola <nikola.stoyanov@ampeco.global>
2024-04-22 15:28:42 +0300

    upd: resource cache buster

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -16,7 +16,7 @@ class FieldServiceProvider extends ServiceProvider
     public function boot()
     {
         Nova::serving(function (ServingNova $event) {
-            Nova::script('nova-ajax-select-2021-04-17', __DIR__.'/../dist/js/field.js');
+            Nova::script('nova-ajax-select-2021-04-22', __DIR__.'/../dist/js/field.js');
         });
     }
 
